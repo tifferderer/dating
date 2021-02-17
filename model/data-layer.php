881c81764cd32c491daa981fa2ce08c1131183c4
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-02-16 20:48:04 -0500

    Data validation file added

---
+++ model/data-layer.php
@@ -0,0 +1,30 @@
+<?php
+
+/* model/data-layer.php
+*returns data for my app
+ *
+*/
+
+function validName() {
+
+}
+
+function validAge() {
+
+}
+
+function validPhone() {
+
+}
+
+function validEmail() {
+
+}
+
+function validOutdoor() {
+
+}
+
+function validIndoor() {
+
+}
