bbc35ae91caf07c9114a77336a49d444bd933459
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-02-16 21:09:17 -0500

    Arrays of interests added

--- model/data-layer.php
+++ model/data-layer.php
@@ -3,3 +3,13 @@
 *returns data for my app
  *
 */
+
+function getIndoorInterests() {
+    return array("tv", "grooming", "sleeping", "snuggles",
+        "treats", "training", "window watching", "hide-n-seek");
+}
+
+function getOutdoorInterests() {
+    return array("fetch", "biking", "chases", "walks",
+        "sunbathing", "swimming", "tug-o-war");
+}
