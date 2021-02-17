38b17447415af8ab5aced7279cf9cd3d805b45e5
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-02-16 21:30:44 -0500

    Validation added into empty methods

--- model/validate.php
+++ model/validate.php
@@ -2,28 +2,49 @@
 
 //contains validation functions
 /** validName() returns true if Name is not empty */
-function validName() {
-
+function validName($name) {
+    return !empty($name && ctype_alpha($name));
 }
 /**validAge returns true if age is between 18 and 118 */
-function validAge() {
-
+function validAge($age) {
+    return($age >= 17 && $age <= 118);
 }
 
-/** validPhone returns true if phone is only numbers and is 9 characters long*/
-function validPhone() {
-
-}
-/** validEmail returns true if email ends in .com */
-function validEmail() {
+/** validPhone returns true if phone is only numbers and is 9 characters long
+ * @param $phone string phone number
+ * @return bool valid number
+ */
+function validPhone($phone) {
+    if(strlen($phone)==10) {
+            return is_numeric($phone);
+        }
+    return false;
+    }
+/** validEmail returns true if email contains @ and . */
+function validEmail($email) {
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    return (filter_var($email, FILTER_VALIDATE_EMAIL));
 
 }
 /** validOutdoor checks each selected outdoor interest against a list of valid options */
-function validOutdoor() {
+function validOutdoor($selected) {
 
+    $validOutside = getOutdoorInterests();
+    foreach ($selected as $outdoor) {
+        if (!(in_array($outdoor, $validOutside))) {
+            return false;
+        }
+    }
+    return true;
 }
 
 /** validIndoor checks each selected indoor interest against a list of valid options */
-function validIndoor() {
-
+function validIndoor($selected) {
+    $validIndoor = getIndoorInterests();
+    foreach ($selected as $indoor) {
+        if (!(in_array($indoor, $validIndoor))) {
+            return false;
+        }
+    }
+    return true;
 }
