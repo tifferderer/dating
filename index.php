7951ac439c8a2a8f6ed4d561a38d4da5a5128f8b
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-02-21 13:36:36 -0500

    Validation and sticky added to profile

--- index.php
+++ index.php
@@ -41,8 +41,6 @@ $f3->route('GET|POST /register', function ($f3) {
         $pname= trim($_POST['pname']);
         $age = trim($_POST['age']);
 
-        // GENDER HAS NOT BEEN SET IN THE SESSION NOR HAS IT BEEN VALIDATED!!!!
-
         //Validate
         if(validName($fname)) {
             $_SESSION['fname'] = $fname;
@@ -92,7 +90,7 @@ $f3->route('GET|POST /register', function ($f3) {
             }
         }
 
-        //if there are no errors, redirect to order2
+        //if there are no errors, redirect
         if(empty($f3->get('errors'))) {
             $f3->reroute('/profile');  //get
         }
@@ -111,58 +109,68 @@ $f3->route('GET|POST /register', function ($f3) {
 });
 
 //define profile form route
-$f3->route('GET|POST /profile', function () {
-    //var_dump($_POST);
-    if(isset($_POST['fname'])) {
-        $_SESSION['fname'] = $_POST['fname'];
+$f3->route('GET|POST /profile', function ($f3) {
+
+    //STATE IS NOT VALIDATED NOR STICKY *****************
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //get required data
+        $email = trim($_POST['email']);
+
+        if (validEmail($email)) {
+            $_SESSION['email'] = $email;
+        } else {
+            $f3->set('errors["email"]', "Email required.");
+        }
+
+        //get optional data
+        if (isset($_POST['seeking'])) {
+
+            $seeking = $_POST['seeking'];
+
+            if (validGender($seeking)) {
+                $_SESSION['seeking'] = $seeking;
+            } else {
+                $f3->set('errors["seeking"]', "Please select a gender.");
+            }
+        }
+
+        if (isset($_POST['bio'])) {
+
+            $bio = trim($_POST['bio']);
+
+            if (!empty($bio)) {
+                $_SESSION['bio'] = $bio;
+            }
+        }
+
+        //if there are no errors, redirect
+        if (empty($f3->get('errors'))) {
+            $f3->reroute('/interests');  //get
+        }
     }
-    if(isset($_POST['lname'])) {
-        $_SESSION['lname'] = $_POST['lname'];
-    }
-    if(isset($_POST['phone'])) {
-        $_SESSION['phone'] = $_POST['phone'];
-    }
-    if(isset($_POST['pname'])) {
-        $_SESSION['pname'] = $_POST['pname'];
-    }
-    if(isset($_POST['age'])) {
-        $_SESSION['age'] = $_POST['age'];
-    }
-    if(isset($_POST['gender'])) {
-        $_SESSION['gender'] = $_POST['gender'];
-    }
+
+    $f3->set('seekings', getGender());
+
+    $f3->set('email', isset($email) ? $email : "");
+    $f3->set('seeking', isset($seeking) ? $seeking : "");
+    $f3->set('bio', isset($bio) ? $bio : "");
+
     $view = new Template();
     echo $view->render('views/profile-form.html');
 });
 
 //define an interests form route
-$f3->route('POST /interests', function () {
-    //var_dump($_POST);
-    if(isset($_POST['email'])) {
-        $_SESSION['email'] = $_POST['email'];
-    }
-    if(isset($_POST['state'])) {
-        $_SESSION['state'] = $_POST['state'];
-    }
-    if(isset($_POST['seeking'])) {
-        $_SESSION['seeking'] = $_POST['seeking'];
-    }
-    if(isset($_POST['bio'])) {
-        $_SESSION['bio'] = $_POST['bio'];
-    }
+$f3->route('GET|POST /interests', function () {
+
     $view = new Template();
     echo $view->render('views/interests-form.html');
 });
 
 //define profile summary route
 $f3->route('POST /summary', function () {
-    //var_dump($_POST);
-    if(isset($_POST['indoor'])) {
-        $_SESSION['indoor'] = implode(" ", $_POST['indoor']);
-    }
-    if(isset($_POST['outdoor'])) {
-        $_SESSION['outdoor'] = implode(" ", $_POST['outdoor']);
-    }
+
     $view = new Template();
     echo $view->render('views/summary.html');
 });
